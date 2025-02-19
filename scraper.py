922bcc1df29de4f23203a278b93c506b2556d0a3
suyashj1231 <149845903+suyashj1231@users.noreply.github.com>
2025-02-18 18:25:36 -0800

    added req domains

--- scraper.py
+++ scraper.py
@@ -25,15 +25,25 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+
+        # our allowed domains
+        allowed_domains = [
+            ".ics.uci.edu",
+            ".cs.uci.edu",
+            ".informatics.uci.edu",
+            ".stat.uci.edu"
+        ]
+        if not any(parsed.netloc.endswith(domain) for domain in allowed_domains):
+            return False
+        if re.match(
+                r".*\.(css|js|bmp|gif|jpg|jpeg|png|pdf|ico"
+                + r"|tiff?|mid|mp2|mp3|mp4|wav|avi|mov|mpeg|m4v|mkv|ogg|ogv"
+                + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx"
+                + r"|data|dat|exe|bz2|tar|msi|bin|7z|dmg|iso"
+                + r"|epub|dll|cnf|tgz|sha1|thmx|mso|arff|rtf|jar|csv"
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$",
+                parsed.path.lower()):
+            return False
 
     except TypeError:
         print ("TypeError for ", parsed)
